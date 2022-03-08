6637a8c7e880c272a7c48113c9044469dc60592a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-03-08 02:15:48 +0100

    alioth: Update
    Change-Id: Ic31d99c7e33816603a99d1022a374bc7a5c49e7f

--- device.mk
+++ device.mk
@@ -18,6 +18,49 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
     fastbootd
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    capture.sh \
+    init.class_main.sh \
+    init.crda.sh \
+    init.mdm.sh \
+    init.mi.usb.sh \
+    init.qcom.class_core.sh \
+    init.qcom.coex.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sdio.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.chg_policy.sh \
+    init.qti.dcvs.sh \
+    init.qti.media.sh \
+    init.qti.qcv.sh \
+    mishow.sh \
+    playback.sh \
+    qca6234-service.sh \
+    setup_backmic2headphone.sh \
+    setup_headsetmic2headphone.sh \
+    setup_mainmic2headphone.sh \
+    setup_rcv2mainmic.sh \
+    setup_rcv2topmic.sh \
+    setup_top-spk2backmic.sh \
+    setup_top-spk2mainmic.sh \
+    setup_top-spk2topmic.sh \
+    setup_topmic2headphone.sh \
+    teardown_loopback.sh \
+    us_cal.sh \
+
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.qcom.factory.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.target.rc \
+    init.target.wigig.rc \
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
