09a9a630acf1a148aeac89b2bcdc016f6057af8d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-03-10 22:19:24 +0100

    alioth: Update
    Change-Id: Ib898d546947000c08e5dc5dfe1cf77e95faf0e5d

--- device.mk
+++ device.mk
@@ -28,6 +28,18 @@ PRODUCT_PACKAGES += \
     update_engine_sideload \
     update_verifier
 
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
 PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
@@ -36,6 +48,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
     fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # Rootdir
 PRODUCT_PACKAGES += \
